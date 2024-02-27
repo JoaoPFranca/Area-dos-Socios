799ee9a21688f0c4bc1d523a5bf24207fef86555
João Pedro de França Barboza <jpbarboza2004@gmail.com>
2024-02-27 09:52:54 -0300

    Auth: Adding cors, login connection

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -3,18 +3,20 @@
     <v-container align="center" justify="center" class="pt-16 mt-16">
       <slot>
         <v-col cols="12" sm="8" md="4">
-          <v-card color="#121212" class="text-center" elevation="8">
-            <v-card-title>
-              <h3 class="display-2">Área dos Sócios</h3>
-            </v-card-title>
-            <v-card-text>
-              <v-text-field type="email" label="Login"/>
-              <v-text-field type="password" label="Senha"/>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn class="bg-secondary" block>Entrar</v-btn>
-            </v-card-actions>
-          </v-card>
+          <v-form>
+            <v-card color="#121212" class="text-center" elevation="8">
+              <v-card-title>
+                <h3 class="display-2">Área dos Sócios</h3>
+              </v-card-title>
+              <v-card-text>
+                <v-text-field v-model="email" type="email" label="Login"/>
+                <v-text-field v-model="password" type="password" label="Senha"/>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="bg-secondary" block @click="this.login">Entrar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-form>
         </v-col>
       </slot>
     </v-container>
@@ -22,6 +24,39 @@
 </template>
 
 <script>
+
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+
+  methods: {
+    async login() {
+      try {
+        const res = await axios.post('http://localhost:3000/api/user/login', {email: this.email, password: this.password});
+        let dados = res.data;
+        localStorage.setItem('token', dados.access_token);
+        this.$router.push('/home');
+      } catch (err) {
+          await Swal.fire({
+            icon: 'error',
+            title: 'Tente novamente!',
+            text: 'Usuário ou senha inválidos!',
+          });
+      }
+    }
+  }
+
+
+
+}
+
 
 </script>
 
